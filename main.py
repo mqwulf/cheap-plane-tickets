7489f1272b3588464cb4f773180cdd888768fc91
Buyantogtokh Khurelbaatar <vicekurel@gmail.com>
2024-11-04 21:04:17 -0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-import requests
-import os
-from dotenv import load_dotenv
-load_dotenv()
+import time
+from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
+from flight_data import FlightData
+
+flight_data = FlightData()
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "SFO"
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        fromTime=tomorrow,
+        toTime=six_months_from_today,
+    )
+    cheapest_flight = flight_data.find_cheapest_flight(flights)
+    print(f"{destination["city"]}: ${cheapest_flight.price}")
+    time.sleep(2)
